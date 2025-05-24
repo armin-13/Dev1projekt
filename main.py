9db550c627590b954f9d456e8c5368e94cb89039
armin-13 <armin6013@gmail.com>
2025-05-24 10:54:58 +0200

    added b11 branch code changes

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ print("Hello Kiri World")
 x = ["ali", "gholi", "jafar", 13, 14, 15]
 for i in x:
     if i == 13:
-     print(x) //from c11 branch code changes
+     print(x) # from b11 code changes + from c11 branch code changes
+     
+
 
 
 
